7e447d1cfb031a5ebe03bf4230fd6b3d3253cd5f
Pankaj Singh Sirari <pankajsirari222@gmail.com>
2023-01-22 01:07:28 +0530

    Meet of two People - Brute & Optimized Approach

--- class1.java
+++ class1.java
@@ -8,9 +8,25 @@ class class1 {
 
         if(willMeet(x, y, s1, s2))  System.out.println("Will Meet");
         else System.out.println("Will not meet");
+
+        if(bruteForce(x, y, s1, s2))    System.out.println("YES");
+        else    System.out.println("NO");
+    }
+    
+    //  Brute Force Approach O(max(x,y))
+    public static boolean bruteForce(int x, int y, int s1, int s2) {
+        if(x > y && s1 > s2 || x < y && s1 < s2)    return false;
+
+        while(Math.abs(x-y) >= 0) {
+            if(x == y)  return true;
+            x += s1;
+            y += s2;
+        }
+        return false;
     }
 
     // x and y are starting points and s1 and s2 are starting speeds.
+    //Optimized Approach
     public static boolean willMeet(int x, int y, int s1, int s2) {
         int netSpeed = Math.abs(s1-s2);
         int netDistance = Math.abs(x-y);
